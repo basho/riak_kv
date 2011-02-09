5d62e1f6980136be395cb329abc729ed16828f51
Kelly McLaughlin <kelly@basho.com>
2011-02-09 12:04:26 -0700

    Check for invalid RW value.

--- src/riak_kv_delete.erl
+++ src/riak_kv_delete.erl
@@ -36,26 +36,30 @@ delete(ReqId,Bucket,Key,RW0,Timeout,Client) ->
     {ok, Ring} = riak_core_ring_manager:get_my_ring(),
     BucketProps = riak_core_bucket:get_bucket(Bucket, Ring),
     N = proplists:get_value(n_val,BucketProps),
-    RW = riak_kv_util:expand_rw_value(rw, RW0, BucketProps, N),
-    {ok,C} = riak:local_client(),
-    case C:get(Bucket,Key,RW,Timeout) of
-        {ok, OrigObj} ->
-            RemainingTime = Timeout - (riak_core_util:moment() - RealStartTime),
-            OrigMD = hd([MD || {MD,_V} <- riak_object:get_contents(OrigObj)]),
-            NewObj = riak_object:update_metadata(OrigObj,
-                            dict:store(<<"X-Riak-Deleted">>, "true", OrigMD)),
-            Reply = C:put(NewObj, RW, RW, RemainingTime),
-            case Reply of
-                ok -> 
-                    spawn(
-                      fun()-> reap(Bucket,Key,RemainingTime) end);
-                _ -> nop
-            end,
-            Client ! {ReqId, Reply};
-        {error, notfound} ->
-            Client ! {ReqId, {error, notfound}};
-        X ->
-            Client ! {ReqId, X}
+    case riak_kv_util:expand_rw_value(rw, RW0, BucketProps, N) of
+        error ->
+            Client ! {ReqId, {error, {rw_val_violation, RW0}}};
+        RW ->
+            {ok,C} = riak:local_client(),
+            case C:get(Bucket,Key,RW,Timeout) of
+                {ok, OrigObj} ->
+                    RemainingTime = Timeout - (riak_core_util:moment() - RealStartTime),
+                    OrigMD = hd([MD || {MD,_V} <- riak_object:get_contents(OrigObj)]),
+                    NewObj = riak_object:update_metadata(OrigObj,
+                                                         dict:store(<<"X-Riak-Deleted">>, "true", OrigMD)),
+                    Reply = C:put(NewObj, RW, RW, RemainingTime),
+                    case Reply of
+                        ok -> 
+                            spawn(
+                              fun()-> reap(Bucket,Key,RemainingTime) end);
+                        _ -> nop
+                    end,
+                    Client ! {ReqId, Reply};
+                {error, notfound} ->
+                    Client ! {ReqId, {error, notfound}};
+                X ->
+                    Client ! {ReqId, X}
+            end                     
     end.
 
 reap(Bucket, Key, Timeout) ->
