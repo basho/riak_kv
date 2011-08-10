e0f77fdbbb9ba12059a9303afa86e40f92763144
Kelly McLaughlin <kelly@basho.com>
2011-08-09 22:56:28 -0600

    Add exported types for fold functions and remove obsolete buffer function.

--- src/riak_kv_backend.erl
+++ src/riak_kv_backend.erl
@@ -23,13 +23,22 @@
 -module(riak_kv_backend).
 
 -export([behaviour_info/1]).
--export([buffer/3]).
 -export([callback_after/3]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -export([standard_test/2]).
 -endif.
+
+-type fold_buckets_fun() :: fun((binary(), any()) -> any() | no_return()).
+-type fold_keys_fun() :: fun((binary(), binary(), any()) -> any() |
+                                                            no_return()).
+-type fold_objects_fun() :: fun((binary(), binary(), term(), any()) ->
+                                       any() |
+                                       no_return()).
+-export_type([fold_buckets_fun/0,
+              fold_keys_fun/0,
+              fold_objects_fun/0]).
 
 -spec behaviour_info(atom()) -> 'undefined' | [{atom(), arity()}].
 behaviour_info(callbacks) ->
@@ -52,19 +61,6 @@ behaviour_info(callbacks) ->
 behaviour_info(_Other) ->
     undefined.
     
-%% @doc Call the buffer function and reset the
-%% buffer if the size of Acc is equal to
-%% the specified buffer size. This function
-%% is used by the individual backend modules
-%% for intermediate buffering during folds.
-buffer(BufferSize, BufferFun, Acc) ->
-    if length(Acc) =:= BufferSize ->
-            BufferFun(Acc),
-            [];
-       true ->
-            Acc
-    end.
-
 %% Queue a callback for the backend after Time ms.
 -spec callback_after(integer(), reference(), term()) -> reference().
 callback_after(Time, Ref, Msg) when is_integer(Time), is_reference(Ref) ->
