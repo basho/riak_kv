d298eba5188965b00589c3aca1891670ebde2b88
Kelly McLaughlin <kelly@basho.com>
2011-08-31 17:03:09 -0600

    Update fold worker to handle bucket folding as well as key folding.

--- src/riak_kv_fold_worker.erl
+++ src/riak_kv_fold_worker.erl
@@ -1,3 +1,28 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2007-2010 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc This module uses the riak_core_vnode_worker behavior to perform 
+%% different riak_kv fold tasks asynchronously.
+
+-author('Kelly McLaughlin <kelly@basho.com>').
+
 -module(riak_kv_fold_worker).
 
 -behaviour(riak_core_vnode_worker).
@@ -9,11 +34,18 @@
 
 -record(state, {index :: partition()}).
 
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+
+%% @doc Initialize the worker. Currently only the VNode index
+%% parameter is used.
 init_worker(VNodeIndex, _Args, _Props) ->
     {ok, #state{index=VNodeIndex}}.
 
+%% @doc Perform the asynchronous fold operation.
 handle_work({Bucket, FoldFun}, From, State) ->
-    io:format("Handling work~n"),
     Acc = try
               FoldFun()
           catch
@@ -25,6 +57,25 @@ handle_work({Bucket, FoldFun}, From, State) ->
                                              {final_results,
                                               {Bucket,
                                                FinalResults}})
+               end,
+    riak_kv_fold_buffer:flush(Acc, FlushFun),
+    {noreply, State};
+handle_work(FoldFun, From, State) ->
+    FoldResults = try
+                      FoldFun()
+                  catch
+                      {break, AccFinal} ->
+                          AccFinal
+                  end,
+    case FoldResults of 
+        {Acc, _} ->
+            ok;
+        Acc ->
+            ok
+    end,
+    FlushFun = fun(FinalResults) ->
+                       riak_core_vnode:reply(From,
+                                             {final_results, FinalResults})
                end,
     riak_kv_fold_buffer:flush(Acc, FlushFun),
     {noreply, State}.
