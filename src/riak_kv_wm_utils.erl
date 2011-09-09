5d0a7dc13f92f39c0b969212ab5d0efc1bf2b35d
Jeffrey Massung <jeff@basho.com>
2011-09-09 09:57:15 -0600

    Using a different encode function so that integers are encoded as strings for the HTTP response.

--- src/riak_kv_wm_utils.erl
+++ src/riak_kv_wm_utils.erl
@@ -124,14 +124,14 @@ multipart_encode_body(Prefix, Bucket, {MD, V}, APIVersion) ->
      case dict:find(?MD_USERMETA, MD) of
          {ok, M} ->
             lists:foldl(fun({Hdr,Val},Acc) ->
-                            [Acc|[Hdr,": ",encode_value(Val),"\r\n"]]
+                            [Acc|[Hdr,": ",encode_2i_value(Val),"\r\n"]]
                         end,
                         [], M);
          error -> []
      end,
      case dict:find(?MD_INDEX, MD) of
          {ok, IF} ->
-             [[?HEAD_INDEX_PREFIX,Key,": ",encode_value(Val),"\r\n"] || {Key,Val} <- IF];
+             [[?HEAD_INDEX_PREFIX,Key,": ",encode_2i_value(Val),"\r\n"] || {Key,Val} <- IF];
          error -> []
      end,
      "\r\n",
@@ -200,6 +200,16 @@ encode_value(V) when is_binary(V) ->
     V;
 encode_value(V) ->
     term_to_binary(V).
+
+%% @spec encode_2i_value(term()) -> binary()
+%% @doc Encode the 2i value as a binary
+encode_2i_value(V) when is_integer(V) ->
+    list_to_binary(erlang:integer_to_list(V));
+encode_2i_value(S) when is_list(S) orelse is_binary(S) ->
+    S;
+encode_2i_value(X) ->
+    throw({unknown_2i_val_type, X}).
+
 
 %% @spec accept_value(string(), binary()) -> term()
 %% @doc Accept the object value as a binary - content type can be used
