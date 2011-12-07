8e11d1147fdc3e0b6a92a1d14bf082909527ee76
Rusty Klophaus <rklophaus@gmail.com>
2011-12-07 14:13:03 -0500

    Fix riak_kv_multi_backend to work with 2i.
    Split api_version/0 into api_version/0 and capabilities/N.  Update
    riak_kv_vnode to inspect capabilities at a bucket level.  Update
    riak_kv_multi_backend to consult child backend when determining
    capabilities.
    
    AZ943

--- src/riak_kv_backend.erl
+++ src/riak_kv_backend.erl
@@ -43,22 +43,25 @@
 
 -spec behaviour_info(atom()) -> 'undefined' | [{atom(), arity()}].
 behaviour_info(callbacks) ->
-    [{api_version,0},
-     {start,2},       % (Partition, Config)
-     {stop,1},        % (State)
-     {get,3},         % (Bucket, Key, State)
-     {put,5},         % (Bucket, Key, IndexSpecs, Val, State)
-     {delete,4},      % (Bucket, Key, IndexSpecs, State)
-     {drop,1},        % (State)
-     {fold_buckets,4},% (FoldBucketsFun, Acc, Opts, State),
-                      %   FoldBucketsFun(Bucket, Acc)
-     {fold_keys,4},   % (FoldKeysFun, Acc, Opts, State),
-                      %   FoldKeysFun(Bucket, Key, Acc)
-     {fold_objects,4},% (FoldObjectsFun, Acc, Opts, State),
-                      %   FoldObjectsFun(Bucket, Key, Object, Acc)
-     {is_empty,1},    % (State)
-     {status,1},      % (State)
-     {callback,3}];   % (Ref, Msg, State) ->
+    [
+     {api_version,0},
+     {capabilities, 1},  % (State)
+     {capabilities, 2},  % (Bucket, State)
+     {start,2},          % (Partition, Config)
+     {stop,1},           % (State)
+     {get,3},            % (Bucket, Key, State)
+     {put,5},            % (Bucket, Key, IndexSpecs, Val, State)
+     {delete,4},         % (Bucket, Key, IndexSpecs, State)
+     {drop,1},           % (State)
+     {fold_buckets,4},   % (FoldBucketsFun, Acc, Opts, State),
+                         %   FoldBucketsFun(Bucket, Acc)
+     {fold_keys,4},      % (FoldKeysFun, Acc, Opts, State),
+                         %   FoldKeysFun(Bucket, Key, Acc)
+     {fold_objects,4},   % (FoldObjectsFun, Acc, Opts, State),
+                         %   FoldObjectsFun(Bucket, Key, Object, Acc)
+     {is_empty,1},       % (State)
+     {status,1},         % (State)
+     {callback,3}];      % (Ref, Msg, State) ->
 behaviour_info(_Other) ->
     undefined.
 
--- src/riak_kv_bitcask_backend.erl
+++ src/riak_kv_bitcask_backend.erl
@@ -27,6 +27,8 @@
 
 %% KV Backend API
 -export([api_version/0,
+         capabilities/1,
+         capabilities/2,
          start/2,
          stop/1,
          get/3,
@@ -68,10 +70,20 @@
 %% ===================================================================
 
 %% @doc Return the major version of the
-%% current API and a capabilities list.
--spec api_version() -> {integer(), [atom()]}.
+%% current API.
+-spec api_version() -> {ok, integer()}.
 api_version() ->
-    {?API_VERSION, ?CAPABILITIES}.
+    {ok, ?API_VERSION}.
+
+%% @doc Return the capabilities of the backend.
+-spec capabilities(state()) -> {ok, [atom()]}.
+capabilities(_) ->
+    {ok, ?CAPABILITIES}.
+
+%% @doc Return the capabilities of the backend.
+-spec capabilities(riak_object:bucket(), state()) -> {ok, [atom()]}.
+capabilities(_, _) ->
+    {ok, ?CAPABILITIES}.
 
 %% @doc Start the bitcask backend
 -spec start(integer(), config()) -> {ok, state()} | {error, term()}.
