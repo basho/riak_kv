07e886231b50f8e5623e4117d9e4027b022a33f4
Ian Plosker <iman1003@gmail.com>
2011-12-14 18:18:54 -0500

    Fixed BZ1306

--- src/riak_kv_get_fsm.erl
+++ src/riak_kv_get_fsm.erl
@@ -350,11 +350,14 @@ update_stats({ok, Obj}, #state{get_usecs = GetUsecs}) ->
     %% especially for objects with large values.
     NumSiblings = riak_object:value_count(Obj),
     Contents = riak_object:get_contents(Obj),
+    ValueSize = fun (Value) when is_binary(Value) -> size(Value);
+                    (Value) -> size(term_to_binary(Value))
+                end,
     ObjSize =
         size(riak_object:bucket(Obj)) +
         size(riak_object:key(Obj)) +
         size(term_to_binary(riak_object:vclock(Obj))) +
-        lists:sum([size(term_to_binary(MD)) + size(Value) || {MD, Value} <- Contents]),
+        lists:sum([size(term_to_binary(MD)) + ValueSize(Value) || {MD, Value} <- Contents]),
     riak_kv_stat:update({get_fsm, undefined, GetUsecs, NumSiblings, ObjSize});
 update_stats(_, #state{get_usecs = GetUsecs}) ->
     riak_kv_stat:update({get_fsm, undefined, GetUsecs, undefined, undefined}).
