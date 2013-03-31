dcb7d5ead7fedf93ff94610604ea4245f10174d5
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-03-31 18:26:30 -0400

    Update to -callback syntax.

--- src/riak_kv_backend.erl
+++ src/riak_kv_backend.erl
@@ -22,7 +22,6 @@
 
 -module(riak_kv_backend).
 
--export([behaviour_info/1]).
 -export([callback_after/3]).
 
 -ifdef(TEST).
@@ -41,29 +40,45 @@
               fold_keys_fun/0,
               fold_objects_fun/0]).
 
--spec behaviour_info(atom()) -> 'undefined' | [{atom(), arity()}].
-behaviour_info(callbacks) ->
-    [
-     {api_version,0},
-     {capabilities, 1},  % (State)
-     {capabilities, 2},  % (Bucket, State)
-     {start,2},          % (Partition, Config)
-     {stop,1},           % (State)
-     {get,3},            % (Bucket, Key, State)
-     {put,5},            % (Bucket, Key, IndexSpecs, Val, State)
-     {delete,4},         % (Bucket, Key, IndexSpecs, State)
-     {drop,1},           % (State)
-     {fold_buckets,4},   % (FoldBucketsFun, Acc, Opts, State),
-                         %   FoldBucketsFun(Bucket, Acc)
-     {fold_keys,4},      % (FoldKeysFun, Acc, Opts, State),
-                         %   FoldKeysFun(Bucket, Key, Acc)
-     {fold_objects,4},   % (FoldObjectsFun, Acc, Opts, State),
-                         %   FoldObjectsFun(Bucket, Key, Object, Acc)
-     {is_empty,1},       % (State)
-     {status,1},         % (State)
-     {callback,3}];      % (Ref, Msg, State) ->
-behaviour_info(_Other) ->
-    undefined.
+-record(state, {
+          default_get = <<>>,
+          default_size = 0,
+          key_count = 0,
+          op_get = 0,
+          op_put = 0,
+          op_delete = 0
+         }).
+-type state() :: #state{}.
+-type config() :: [{atom(), term()}].
+-type index_spec() :: {add, Index, SecondaryKey} | {remove, Index, SecondaryKey}.
+
+-callback api_version() -> {ok, integer()}.
+-callback capabilities(state()) -> {ok, [atom()]}.
+-callback capabilities(riak_object:bucket(), state()) -> {ok, [atom()]}.
+-callback start(integer(), config()) -> {ok, state()} | {error, term()}.
+-callback stop(state()) -> ok.
+-callback get(riak_object:bucket(), riak_object:key(), state()) ->
+                 {ok, any(), state()}.
+-callback put(riak_object:bucket(), riak_object:key(),
+              [index_spec()], binary(), state()) -> {ok, state()}.
+-callback delete(riak_object:bucket(), riak_object:key(), [index_spec()], state()) ->
+                    {ok, state()}.
+-callback fold_buckets(riak_kv_backend:fold_buckets_fun(),
+                   any(),
+                   [],
+                   state()) -> {ok, any()}.
+-callback fold_keys(riak_kv_backend:fold_keys_fun(),
+                any(),
+                [{atom(), term()}],
+                state()) -> {ok, term()}.
+-callback fold_objects(riak_kv_backend:fold_objects_fun(),
+                   any(),
+                   [{atom(), term()}],
+                   state()) -> {ok, any()} | {async, fun()}.
+-callback drop(state()) -> {ok, state()}.
+-callback is_empty(state()) -> false.
+-callback status(state()) -> [{atom(), term()}].
+-callback callback(reference(), any(), state()) -> {ok, state()}.
 
 %% Queue a callback for the backend after Time ms.
 -spec callback_after(integer(), reference(), term()) -> reference().
