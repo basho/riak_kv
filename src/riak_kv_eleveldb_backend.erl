basho/riak_kv

commit b50d1df73aee20c4260e72d73fdfcf6493603636

move querying of fixed index status to seperate backend function
makes top-level fixed_indexes status on multi-backend,
which messes with stats, unncessary allowing it to be removed

src/riak_kv_eleveldb_backend.erl
@@ -36,6 +36,7 @@
          fix_index/3,
          mark_indexes_fixed/2,
          set_legacy_indexes/2,
+         fixed_index_status/1,
          fold_buckets/4,
          fold_keys/4,
          fold_objects/4,
@@ -278,6 +279,10 @@ mark_indexes_fixed(State=#state{ref=Ref, write_opts=WriteOpts}, ForUpgrade) ->
 
 set_legacy_indexes(State, WriteLegacy) ->
     State#state{legacy_indexes=WriteLegacy}.
+
+-spec fixed_index_status(state()) -> boolean().
+fixed_index_status(#state{fixed_indexes=Fixed}) ->
+    Fixed.
 
 %% @doc Delete an object from the eleveldb backend
 -spec delete(riak_object:bucket(), riak_object:key(), [index_spec()], state()) ->

src/riak_kv_multi_backend.erl
@@ -41,7 +41,8 @@
          callback/3,
          fix_index/3,
          set_legacy_indexes/2,
-         mark_indexes_fixed/2]).
+         mark_indexes_fixed/2,
+         fixed_index_status/1]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -322,31 +323,7 @@ status(#state{backends=Backends}) ->
     %% breaking this API list of two tuples return,
     %% add the tuple {mod, Mod} to the status for each
     %% backend.
-    {Statuses, FixedIndexes} =
-        lists:mapfoldl(fun({N, Mod, ModState}, Acc) ->
-                               Status = Mod:status(ModState),
-                               Entry = {N, [{mod, Mod} | Status]},
-                               case fixed_index_status(Mod, ModState, Status) of
-                                   undefined -> {Entry, Acc};
-                                   Res ->
-                                       case Acc of
-                                           undefined -> {Entry, Res};
-                                           _ -> {Entry, Res andalso Acc}
-                                       end
-                               end
-                       end,
-                       undefined,
-                       Backends),
-    case FixedIndexes of
-        undefined -> Statuses;
-        _ -> [{fixed_indexes, FixedIndexes} | Statuses]
-    end.
-
-fixed_index_status(Mod, ModState, Status) ->
-    case backend_can_index_reformat(Mod, ModState) of
-        true -> proplists:get_value(fixed_indexes, Status);
-        false -> undefined
-    end.
+    [{N, [{mod, Mod} | Mod:status(ModState)]} || {N, Mod, ModState} <- Backends].
 
 %% @doc Register an asynchronous callback
 -spec callback(reference(), any(), state()) -> {ok, state()}.
@@ -415,6 +392,30 @@ backend_fix_index({_, Mod, ModState}, Bucket, StorageKey, ForUpgrade) ->
                        [Bucket, StorageKey, Mod, Reason]),
             {0, 0, length(StorageKey)}
     end.
+
+-spec fixed_index_status(state()) -> boolean().
+fixed_index_status(#state{backends=Backends}) ->
+    lists:foldl(fun({_N, Mod, ModState}, Acc) ->
+                        Status = Mod:status(ModState),
+                        case fixed_index_status(Mod, ModState, Status) of
+                            undefined -> Acc;
+                            Res ->
+                                case Acc of
+                                    undefined -> Res;
+                                    _ -> Res andalso Acc
+                                end
+                        end
+                end,
+                undefined,
+                Backends).
+
+fixed_index_status(Mod, ModState, Status) ->
+    case backend_can_index_reformat(Mod, ModState) of
+        true -> proplists:get_value(fixed_indexes, Status);
+        false -> undefined
+    end.
+
+
 
 %% ===================================================================
 %% Internal functions

src/riak_kv_vnode.erl
@@ -550,8 +550,8 @@ handle_command({get_index_entries, Opts},
     case lists:member(index_reformat, Caps) of
         true ->
             ModState = Mod:set_legacy_indexes(ModState0, not ForUpgrade),
-            Status = Mod:status(ModState),
-            case {ForUpgrade, proplists:get_value(fixed_indexes, Status)} of
+            Status = Mod:fixed_index_status(ModState),
+            case {ForUpgrade, Status} of
                 {true, true} -> {reply, done, State};
                 {_,  _} ->
                     BufferMod = riak_kv_fold_buffer,

commit 4fd31a0ce3beb400eeb975747fbf3ee618d41eab

fix regression in eleveldb fixed indexes flag
introduced by b50d1df73a. the check assumed that is_empty
would return false when the fixed indexes key was written.
Since the change to is_empty accounts for this key and ignores it
lets just call the old version (eleveldb:is_empty) directly.
see comment in patch for more.

src/riak_kv_eleveldb_backend.erl
@@ -120,7 +120,12 @@ determine_fixed_index_status(State) ->
         true ->
             {ok, State#state{fixed_indexes=true}};
         false ->
-            case is_empty(State) of
+            %% call eleveldb directly to circumvent extra check
+            %% for fixed indexes entry. if entry is present we
+            %% don't want to ignore becasue it occurs on downgrade.
+            %% ignoring is not dangerous but reports confusing results
+            %% (empty downgraded partitions still returning fixed = true)
+            case eleveldb:is_empty(State#state.ref) of
                 true -> mark_indexes_fixed_on_start(State);
                 false -> {ok, State#state{fixed_indexes=false}}
             end