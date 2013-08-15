0e9c1916418dbc669b183dce54832b52dc6d39f9
Andrew Thompson <andrew@hijacked.us>
2013-08-15 11:38:23 -0400

    Binaries are our friend

--- src/riak_kv_wm_object.erl
+++ src/riak_kv_wm_object.erl
@@ -205,7 +205,8 @@ is_authorized(ReqData, Ctx) ->
                     case wrq:get_req_header("Authorization", ReqData) of
                         "Basic " ++ Base64 ->
                             UserPass = base64:decode_to_string(Base64),
-                            [User, Pass] = string:tokens(UserPass, ":"),
+                            [User, Pass] = [list_to_binary(X) || X <-
+                                                                 string:tokens(UserPass, ":")],
                             {ok, Peer} = inet_parse:address(wrq:peer(ReqData)),
                             case riak_core_security:authenticate(User, Pass,
                                     [{ip, Peer}])
@@ -268,7 +269,7 @@ forbidden(RD, Ctx=#ctx{security=Security}) ->
             end,
 
             {Res, _} = riak_core_security:check_permission(Perm,
-                                                           binary_to_list(Ctx#ctx.bucket),
+                                                           Ctx#ctx.bucket,
                                                            Security),
             case Res of
                 false ->
--- src/riak_kv_wm_ping.erl
+++ src/riak_kv_wm_ping.erl
@@ -45,7 +45,8 @@ is_authorized(ReqData, Ctx) ->
                     case wrq:get_req_header("Authorization", ReqData) of
                         "Basic " ++ Base64 ->
                             UserPass = base64:decode_to_string(Base64),
-                            [User, Pass] = string:tokens(UserPass, ":"),
+                            [User, Pass] = [list_to_binary(X) || X <-
+                                                                 string:tokens(UserPass, ":")],
                             {ok, Peer} = inet_parse:address(wrq:peer(ReqData)),
                             case riak_core_security:authenticate(User, Pass,
                                                                  [{ip, Peer}])
