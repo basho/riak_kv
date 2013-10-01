771c87af1ccd316af179481a00040e3a5bbf77c7
Scott Lystig Fritchie <slfritchie@snookles.com>
2013-10-01 16:40:36 +0900

    Remove hashtree:compare/2 and hashtree:compare/3 functions.
    These functions are only used by tests and by developer
    scaffolding.  See also:
    https://github.com/basho/riak_kv/pull/646#discussion_r6673687

--- src/hashtree.erl
+++ src/hashtree.erl
@@ -90,14 +90,14 @@
 %% to LevelDB using a single batch write. Writes are flushed whenever the
 %% buffer becomes full, as well as before updating the hashtree.
 %%
-%% Tree exchange is provided by the ``compare/2'' and ``compare/3'' functions.
-%% The behavior of these functions is determined through a provided function
+%% Tree exchange is provided by the ``compare/4'' function.
+%% The behavior of this function is determined through a provided function
 %% that implements logic to get buckets and segments for a given remote tree,
 %% as well as a callback invoked as key differences are determined. This
 %% generic interface allows for tree exchange to be implemented in a variety
 %% of ways, including directly against to local hash tree instances, over
 %% distributed Erlang, or over a custom protocol over a TCP socket. See
-%% ``local_compare/2'' and ``do_remote/1'' for examples.
+%% ``local_compare/2'' and ``do_remote/1'' for examples (-ifdef(TEST) only).
 
 -module(hashtree).
 
@@ -114,9 +114,6 @@
          destroy/1,
          read_meta/2,
          write_meta/3,
-         local_compare/2,
-         compare/2,
-         compare/3,
          compare/4,
          top_hash/1,
          get_bucket/3,
@@ -126,6 +123,7 @@
          width/1,
          mem_levels/1]).
 
+-ifdef(TEST).
 -export([run_local/0,
          run_local/1,
          run_concurrent_build/0,
@@ -134,6 +132,7 @@
          run_multiple/2,
          run_remote/0,
          run_remote/1]).
+-endif. % TEST
 
 -ifdef(EQC).
 -export([prop_correct/0]).
@@ -383,26 +382,6 @@ rehash_perform(State) ->
 -spec top_hash(hashtree()) -> [] | [{0, binary()}].
 top_hash(State) ->
     get_bucket(1, 0, State).
-
--spec local_compare(hashtree(), hashtree()) -> [keydiff()].
-local_compare(T1, T2) ->
-    Remote = fun(get_bucket, {L, B}) ->
-                     get_bucket(L, B, T2);
-                (key_hashes, Segment) ->
-                     [{_, KeyHashes2}] = key_hashes(T2, Segment),
-                     KeyHashes2
-             end,
-    compare(T1, Remote).
-
--spec compare(hashtree(), remote_fun()) -> [keydiff()].
-compare(Tree, Remote) ->
-    compare(Tree, Remote, fun(Keys, KeyAcc) ->
-                                  Keys ++ KeyAcc
-                          end).
-
--spec compare(hashtree(), remote_fun(), acc_fun(X)) -> X.
-compare(Tree, Remote, AccFun) ->
-    compare(Tree, Remote, AccFun, []).
 
 compare(Tree, Remote, AccFun, Acc) ->
     compare(1, 0, Tree, Remote, AccFun, Acc).
@@ -897,6 +876,8 @@ expand(V, N, Acc) ->
 %%% Experiments
 %%%===================================================================
 
+-ifdef(TEST).
+
 run_local() ->
     run_local(10000).
 run_local(N) ->
@@ -1050,7 +1031,25 @@ peval(L) ->
 %%% EUnit
 %%%===================================================================
 
--ifdef(TEST).
+-spec local_compare(hashtree(), hashtree()) -> [keydiff()].
+local_compare(T1, T2) ->
+    Remote = fun(get_bucket, {L, B}) ->
+                     get_bucket(L, B, T2);
+                (key_hashes, Segment) ->
+                     [{_, KeyHashes2}] = key_hashes(T2, Segment),
+                     KeyHashes2
+             end,
+    compare(T1, Remote).
+
+-spec compare(hashtree(), remote_fun()) -> [keydiff()].
+compare(Tree, Remote) ->
+    compare(Tree, Remote, fun(Keys, KeyAcc) ->
+                                  Keys ++ KeyAcc
+                          end).
+
+-spec compare(hashtree(), remote_fun(), acc_fun(X)) -> X.
+compare(Tree, Remote, AccFun) ->
+    compare(Tree, Remote, AccFun, []).
 
 %% Verify that `update_tree/1' generates a snapshot of the underlying
 %% LevelDB store that is used by `compare', therefore isolating the
