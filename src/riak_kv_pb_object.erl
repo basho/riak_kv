da13df2101b2c57545c88af88cef9eed0821617b
Joseph Blomstedt <joe@basho.com>
2013-10-21 17:59:57 -0700

    Update PB client to support consistent operations

--- src/riak_kv_pb_object.erl
+++ src/riak_kv_pb_object.erl
@@ -162,7 +162,17 @@ process(#rpbputreq{bucket=B, key=K, vclock=PbVC,
         #state{client=C} = State) when NotMod; NoneMatch ->
     GetOpts = make_option(n_val, N_val) ++
               make_option(sloppy_quorum, SloppyQuorum),
-    case C:get(B, K, GetOpts) of
+    Result = case riak_kv_util:consistent_object(B) of
+                 true ->
+                     consistent;
+                 false ->
+                     C:get(B, K, GetOpts)
+             end,
+    case Result of
+        consistent ->
+            process(Req#rpbputreq{if_not_modified=undefined,
+                                  if_none_match=consistent},
+                    State);
         {ok, _} when NoneMatch ->
             {error, "match_found", State};
         {ok, O} when NotMod ->
@@ -187,7 +197,8 @@ process(#rpbputreq{bucket=B, key=K, vclock=PbVC,
 process(#rpbputreq{bucket=B, key=K, vclock=PbVC, content=RpbContent,
                    w=W0, dw=DW0, pw=PW0, return_body=ReturnBody,
                    return_head=ReturnHead, timeout=Timeout, asis=AsIs,
-                   n_val=N_val, sloppy_quorum=SloppyQuorum},
+                   n_val=N_val, sloppy_quorum=SloppyQuorum,
+                   if_none_match=NoneMatch},
         #state{client=C} = State) ->
 
     case K of
@@ -216,10 +227,16 @@ process(#rpbputreq{bucket=B, key=K, vclock=PbVC, content=RpbContent,
                           _ -> []
                       end
               end,
+    Options2 = case NoneMatch of
+                   consistent ->
+                       [{if_none_match, true}|Options];
+                   _ ->
+                       Options
+               end,
     case C:put(O, make_options([{w, W}, {dw, DW}, {pw, PW}, 
                                 {timeout, Timeout}, {asis, AsIs},
                                 {n_val, N_val},
-                                {sloppy_quorum, SloppyQuorum}]) ++ Options) of
+                                {sloppy_quorum, SloppyQuorum}]) ++ Options2) of
         ok when is_binary(ReturnKey) ->
             PutResp = #rpbputresp{key = ReturnKey},
             {reply, PutResp, State};
