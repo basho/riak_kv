96c0e3320c9f62f45de9926cf34306f250796640
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-03-28 16:22:42 -0700

    Ignore the result of the list comprehension.
    Resolves:
    
    riak_kv_stat.erl:310: Expression produces a value of type [any()], but this value is unmatched

--- src/riak_kv_stat.erl
+++ src/riak_kv_stat.erl
@@ -307,7 +307,7 @@ do_get_bucket(true, {Bucket, Microsecs, Stages, NumSiblings, ObjSize}=Args) ->
             do_stages([?APP, node, gets, time, Bucket], Stages);
         {'EXIT', _} ->
             ok = folsom_metrics:new_spiral({?APP, node, gets, Bucket}),
-            [register_stat({?APP, node, gets, Dimension, Bucket}, histogram) || Dimension <- [time,
+            _ = [register_stat({?APP, node, gets, Dimension, Bucket}, histogram) || Dimension <- [time,
                                                                                   siblings,
                                                                                   objsize]],
             do_get_bucket(true, Args)
