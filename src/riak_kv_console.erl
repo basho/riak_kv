742432e6170e4266a441c2df88445aadb661c32b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-03-28 17:01:58 -0700

    Assert we can load riak_core.
    Resolves:
    
    riak_kv_console.erl:219: Expression produces a value of type 'ok' | {'error',_}, but this value is unmatched

--- src/riak_kv_console.erl
+++ src/riak_kv_console.erl
@@ -216,7 +216,7 @@ reip([OldNode, NewNode]) ->
         %%
         %% Do *not* convert to use riak_core_ring_manager:ring_trans.
         %%
-        application:load(riak_core),
+        ok = application:load(riak_core),
         RingStateDir = app_helper:get_env(riak_core, ring_state_dir),
         {ok, RingFile} = riak_core_ring_manager:find_latest_ringfile(),
         BackupFN = filename:join([RingStateDir, filename:basename(RingFile)++".BAK"]),
