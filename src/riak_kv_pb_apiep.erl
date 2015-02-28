ed11373ff4398273bd6817d27e9eca9d1e7e87ef
Andrei Zavada <johnhommer@gmail.com>
2015-02-28 03:38:10 +0200

    DP-6 API entry points coverage, http and pbc API, cached
    There are accompanying changes in newly created branches:
    
     * riak_pb/feature/az/api-entrypoints-coverage, and
     * riak_api/feature/az/api-entrypoints-coverage
    
    that will have to be merged first.  Python client changes are in
    
     * riak-python-client/feature/az/api-entrypoints-coverage.
    
    The following HTTP requests are accepted (values for parameter P are
    "http", "pbc", assuming the latter by default; for F, values are "1"
    and "0", the latter by default):
    
    1) /ring/coverage?proto=P&force_update=F
    
      returning a JSON like this:
    
       {"struct":{"addr":"192.168.1.10","port":10017,"last_checked":1426041629},
        "struct":...}
    
    providing a complete set of API entry points into the cluster via
    protocol P. The value of 'last_checked' is a unixtime of the last
    actual call to getifaddrs/0 used to get the bound address.
    
    2) /ring/coverage/bucket/B/key/K?proto=P&force_update=F
    
    returning a similar JSON with details as above.  There will be a
    single element in the list, in which addr:port is where an entry point
    is for shortest data access to the key K in bucket B, via the given
    protocol API.
    
    On the first call, or with force_update=1, results are cached in
    cluster metadata.  There is no expiry period; clients finding their
    connections dropped should re-request interesting entry points with
    force_update=1.
    
    HTTP responses are further cached on their own, until the next
    multiple of (a default of) 15 secs, configurable via parameter
    'ring_coverage_cache_expiry_time'.
    
    For pb, two new messages are provided:
    
     RpbApiEpReq (code 90),
     RpbApiEpResp(code 91).
    
    declared in riak_pb/src/riak_kv.proto.
    
    The coverage information is obtained in two steps:
    
    1. using riak_core_apl:get_apl_ann(), get active preflist for given
       Bucket and Key, and extract riak nodes from it;
    
    2. make an rpc call on those nodes to:
    
       (a) determine which of the relevant listener apps (i.e., riak_api_pb_listener,
           riak_api_web) are running on those nodes,
           and find the ports these are listening on.
    
       (b) get the bound ip address of the first non-loopback interface on the
           underlying os (all code is in riak_api/src/riak_api_lib.erl).
    
    The corresponding new riak python client method is:
    
    * client.get_api_entry_points(bucket, key, proto = 'pbc', force_update = False)
    
    where bucket and key can be None to request the complete map.

---
+++ src/riak_kv_pb_apiep.erl
@@ -0,0 +1,99 @@
+%% -------------------------------------------------------------------
+%%
+%% riak_api_pb_apiep: Protobuff callbacks providing a `location service'
+%%                    to external clients for optimal access to hosts
+%%                    with partitions containing known buckets/key
+%%
+%% Copyright (c) 2015 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc Protobuff callbacks providing a `location service' to external
+%%      clients for optimal access to hosts with partitions (all or a
+%%      subset containing certain buckets/key).
+%%
+%%  This module serves requests (code), returning response (code):
+%%
+%%    RpbApiEpReq (90) -> RpbApiEpResp (91)
+%%
+%%  If parameter force_update is specified and has a value of 1, this
+%%  information is collected via (expensive) rpc calls to all riak_kv
+%%  nodes; else, values cached in cluster metadata from a previous
+%%  call are returned.
+%%
+
+-module(riak_kv_pb_apiep).
+
+-behaviour(riak_api_pb_service).
+
+-export([init/0,
+         decode/2,
+         encode/1,
+         process/2,
+         process_stream/3]).
+
+-include_lib("riak_pb/include/riak_kv_pb.hrl").
+
+-define(plget, proplists:get_value).
+
+
+-spec init() -> undefined.
+init() ->
+    undefined.
+
+decode(Code, Bin) when Code == 90 ->
+    Msg = riak_pb_codec:decode(Code, Bin),
+    case Msg of
+        #rpbapiepreq{bkey = undefined,
+                     proto = P,
+                     force_update = ForceUpdate} ->
+            {ok, Msg, {"riak_kv.apiep", {{<<>>, <<>>}, P, ForceUpdate}}};
+        #rpbapiepreq{bkey = #rpbbucketkey{bucket = B, key = K},
+                     proto = P,
+                     force_update = ForceUpdate} ->
+            {ok, Msg, {"riak_kv.apiep", {{B, K}, P, ForceUpdate}}}
+    end.
+
+encode(Message) ->
+    {ok, riak_pb_codec:encode(Message)}.
+
+
+process(#rpbapiepreq{bkey = #rpbbucketkey{bucket = Bucket, key = Key},
+                     proto = Proto,
+                     force_update = ForceUpdate}, State) ->
+    EPList = riak_kv_apiep:get_entrypoints(
+               Proto, [{bkey, {Bucket, Key}},
+                       {force_update, ForceUpdate}]),
+    APList = lists:map(fun(EP) -> {?plget(addr, EP),
+                                   ?plget(port, EP),
+                                   ?plget(last_checked, EP)} end, EPList),
+    {reply, #rpbapiepresp{
+               eplist = [#rpbapiep{
+                            addr = if Addr == not_routed -> <<"not_routed">>;
+                                      el/=se -> list_to_binary(inet:ntoa(Addr)) end,
+                            port = Port,
+                            last_checked = unixtime(LastChecked)} ||
+                            {Addr, Port, LastChecked} <- APList]},
+     State}.
+
+
+process_stream(_, _, State) ->
+    {ignore, State}.
+
+%% @private
+unixtime({NowMega, NowSec, _}) ->
+    NowMega * 1000 * 1000 + NowSec.
