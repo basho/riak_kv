fe4eaa3eb31a6eb6e6ae7931f9f58ecfe22c12b0
Fred Dushin <fdushin@basho.com>
2015-03-26 23:17:03 -0400

    Fixed issue with dialyzer -- async_put will always return ok.

--- src/riak_kv_eleveldb_backend.erl
+++ src/riak_kv_eleveldb_backend.erl
@@ -206,12 +206,8 @@ put(Bucket, PrimaryKey, IndexSpecs, Val, #state{ref=Ref,
 
 async_put(Context, Bucket, PrimaryKey, Val, #state{ref=Ref, write_opts=WriteOpts}=State) ->
     StorageKey = to_object_key(Bucket, PrimaryKey),
-    case eleveldb:async_put(Ref, Context, StorageKey, Val, WriteOpts) of
-        ok ->
-            {ok, State};
-        {error, Reason} ->
-            {error, Reason, State}
-    end.
+    eleveldb:async_put(Ref, Context, StorageKey, Val, WriteOpts),
+    {ok, State}.
 
 indexes_fixed(#state{ref=Ref,read_opts=ReadOpts}) ->
     case eleveldb:get(Ref, to_md_key(?FIXED_INDEXES_KEY), ReadOpts) of
