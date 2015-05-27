19e8ff31163ea3fe63a5053629978a1f4ad7ce6d
John R. Daily <git@epep.us>
2015-05-27 10:09:04 -0400

    Removing cover has exposed a problem: riak_kv_backend in rebar's erl_first_files is necessary to make the behavior work, but causes problems with rebar and eunit. The non-test version of riak_kv_backend is being invoked when other backends run riak_kv_backend:standard_test, and that function is not exposed without the TEST macro definition.
    Refactor the test code out of riak_kv_backend into a new file
    test/backend_test_util.erl.
    
    Also eliminated some superfluous whitespace along the way.

--- src/riak_kv_eleveldb_backend.erl
+++ src/riak_kv_eleveldb_backend.erl
@@ -259,9 +259,9 @@ fix_index(IndexKey, ForUpgrade, #state{ref=Ref,
 fix_index(IndexKey, ForUpgrade, Ref, ReadOpts, WriteOpts) ->
     case eleveldb:get(Ref, IndexKey, ReadOpts) of
         {ok, _} ->
-            case from_index_key(IndexKey) of 
+            case from_index_key(IndexKey) of
                 {Bucket, Key, Field, Value} ->
-                    
+
                     NewKey = case ForUpgrade of
                                  true -> to_index_key(Bucket, Key, Field, Value);
                                  false -> to_legacy_index_key(Bucket, Key, Field, Value)
@@ -895,7 +895,7 @@ to_object_key(Bucket, Key) ->
 
 from_object_key(LKey) ->
     case (catch sext:decode(LKey)) of
-        {'EXIT', _} -> 
+        {'EXIT', _} ->
             lager:warning("Corrupted object key, discarding"),
             ignore;
         {o, Bucket, Key} ->
@@ -912,7 +912,7 @@ to_legacy_index_key(Bucket, Key, Field, Term) -> %% encode with legacy bignum en
 
 from_index_key(LKey) ->
     case (catch sext:decode(LKey)) of
-        {'EXIT', _} -> 
+        {'EXIT', _} ->
             lager:warning("Corrupted index key, discarding"),
             ignore;
         {i, Bucket, Field, Term, Key} ->
@@ -934,12 +934,12 @@ to_md_key(Key) ->
 simple_test_() ->
     ?assertCmd("rm -rf test/eleveldb-backend"),
     application:set_env(eleveldb, data_root, "test/eleveldb-backend"),
-    riak_kv_backend:standard_test(?MODULE, []).
+    backend_test_util:standard_test(?MODULE, []).
 
 custom_config_test_() ->
     ?assertCmd("rm -rf test/eleveldb-backend"),
     application:set_env(eleveldb, data_root, ""),
-    riak_kv_backend:standard_test(?MODULE, [{data_root, "test/eleveldb-backend"}]).
+    backend_test_util:standard_test(?MODULE, [{data_root, "test/eleveldb-backend"}]).
 
 retry_test_() ->
     {spawn, [fun retry/0, fun retry_fail/0]}.
