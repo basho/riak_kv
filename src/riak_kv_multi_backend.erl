db9e2ab0a372ec7f4bee59460bfd1b6e23fb5216
John R. Daily <git@epep.us>
2015-05-27 10:09:04 -0400

    Removing cover has exposed a problem: riak_kv_backend in rebar's erl_first_files is necessary to make the behavior work, but causes problems with rebar and eunit. The non-test version of riak_kv_backend is being invoked when other backends run riak_kv_backend:standard_test, and that function is not exposed without the TEST macro definition.
    Refactor the test code out of riak_kv_backend into a new file
    test/backend_test_util.erl.
    
    Also eliminated some superfluous whitespace along the way.

--- src/riak_kv_multi_backend.erl
+++ src/riak_kv_multi_backend.erl
@@ -389,18 +389,18 @@ maybe_mark_indexes_fixed(Mod, ModState, ForUpgrade) ->
     end.
 
 fix_index(BKeys, ForUpgrade, State) ->
-    % Group keys per bucket 
+    % Group keys per bucket
     PerBucket = lists:foldl(fun(BK={B,_},D) -> dict:append(B,BK,D) end, dict:new(), BKeys),
-    Result = 
+    Result =
         dict:fold(
             fun(Bucket, StorageKey, Acc = {Success, Ignore, Errors}) ->
                 {_, Mod,  ModState} = Backend = get_backend(Bucket, State),
                 case backend_can_index_reformat(Mod, ModState) of
-                    true -> 
-                            {S, I, E} = backend_fix_index(Backend, Bucket, 
+                    true ->
+                            {S, I, E} = backend_fix_index(Backend, Bucket,
                                                           StorageKey, ForUpgrade),
                             {Success + S, Ignore + I, Errors + E};
-                    false -> 
+                    false ->
                             Acc
                 end
             end, {0, 0, 0}, PerBucket),
@@ -408,7 +408,7 @@ fix_index(BKeys, ForUpgrade, State) ->
 
 backend_fix_index({_, Mod, ModState}, Bucket, StorageKey, ForUpgrade) ->
     case Mod:fix_index(StorageKey, ForUpgrade, ModState) of
-        {reply, Reply, _UpModState} -> 
+        {reply, Reply, _UpModState} ->
             Reply;
         {error, Reason} ->
            lager:error("Failed to fix index for bucket ~p, key ~p, backend ~p: ~p",
@@ -651,7 +651,7 @@ multi_backend_test_() ->
 
                        %% Run the standard backend test...
                        Config = sample_config(),
-                       riak_kv_backend:standard_test(?MODULE, Config)
+                       backend_test_util:standard_test(?MODULE, Config)
                end
               }
       end,
