d4cc11da1e562ce53d36a8af4a5e812c86957688
Bryce Kerley <bkerley@brycekerley.net>
2015-07-22 19:05:21 -0400

    build out more pb interface

--- src/riak_kv_pb_timeseries.erl
+++ src/riak_kv_pb_timeseries.erl
@@ -1,11 +1,15 @@
 -module(riak_kv_pb_timeseries).
 
+-include_lib("riak_pb/include/riak_kv_pb.hrl").
+-include_lib("riak_ql/include/riak_ql_sql.hrl").
+-include_lib("riak_ql/include/riak_ql_ddl.hrl").
 -behaviour(riak_api_pb_service).
 
 -export([init/0,
          decode/2,
          encode/1,
-         process/2]).
+         process/2,
+         process_stream/3]).
 
 -record(state, {}).
 
@@ -16,12 +20,34 @@ init() ->
 decode(Code, Bin) ->
     Msg = riak_pb_codec:decode(Code, Bin),
     case Msg of
-        #tsqueryreq{query = Q}->
-            {ok, Decoded, Table} = decode_query(Q),
-            {ok, Decoded, {"riak_kv.ts_query", Table}};
-        #tsputreq{table = Table, columns = Columns, rows = Rows} ->
-            {ok, prepare_put(Table, Columns, Rows), {"riak_kv.ts_put", Table}}
+        #tsqueryreq{query=Q}->
+            {ok, DecodedQuery} = decode_query(Q),
+            PermAndTarget = decode_query_permissions(DecodedQuery),
+            {ok, DecodedQuery, PermAndTarget};
+        #tsputreq{table=Table, columns=Columns, rows=Rows} ->
+            {ok, Msg, {"riak_kv.ts_put", Table}}
     end.
 
 encode(Message) ->
     {ok, riak_pb_codec:encode(Message)}.
+
+process(#tsputreq{table=_Table, columns=_Columns, rows=_Rows}, State) ->
+    {reply, #tsputresp{}, State};
+process(_Ddl = #ddl_v1, State) ->
+    {reply, #tsqueryresp{}, State};
+process({_DecodedQuery}, State) ->
+    {reply, #tsqueryresp{}, State}.
+
+process_stream(_, _, State)->
+    {ignore, State}.
+
+decode_query(Query) ->
+    Query#tsinterpolation{base=BaseQuery, interpolations=Interpolations},
+    Lexed = riak_ql_lexer:get_tokens(BaseQuery),
+    {ok, Parsed} = riak_ql_parser:parse(Lexed),
+    Parsed.
+
+decode_query_permissions(#ddl_v1{bucket=NewBucket}) ->
+    {"riak_kv.ts_create_table", NewBucket};
+decode_query_permissions(#riak_sql_v1{'FROM'=Bucket}) ->
+    {"riak_kv.ts_query", Bucket}.
