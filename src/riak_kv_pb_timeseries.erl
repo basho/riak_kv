784f9cf2a5255f6e2658d2e3ec5604f71cab1a14
Gordon Guthrie <gguthrie@basho.com>
2015-09-07 07:59:38 +0000

    added some protocol buffer handling code

--- src/riak_kv_pb_timeseries.erl
+++ src/riak_kv_pb_timeseries.erl
@@ -31,7 +31,8 @@ decode(Code, Bin) ->
 encode(Message) ->
     {ok, riak_pb_codec:encode(Message)}.
 
-process(#tsputreq{table=_Table, columns=_Columns, rows=_Rows}, State) ->
+process(#tsputreq{table=_Table, columns=Columns, rows=Rows}, State) ->
+    Data = make_data(Rows, Columns),
     {reply, tsputresp, State};
 process(_Ddl = #ddl_v1{}, State) ->
     {reply, #tsqueryresp{
@@ -63,3 +64,71 @@ decode_query_permissions(#ddl_v1{bucket=NewBucket}) ->
     {"riak_kv.ts_create_table", NewBucket};
 decode_query_permissions(#riak_sql_v1{'FROM'=Bucket}) ->
     {"riak_kv.ts_query", Bucket}.
+
+make_data(Rows, Cols) ->
+    make_d2(Rows, Cols, []).
+
+make_d2([], _Cols, Acc) ->
+    lists:reverse(Acc);
+make_d2([{tsrow, Row} | T], Cols, Acc) ->
+    gg:format("Row is ~p~n- Cols is ~p~n", [Row, Cols]),
+    make_d2(T, Cols, [make_d3(Row, Cols, []) | Acc]).
+
+make_d3([], _Cols, Acc) ->
+    list_to_tuple(lists:reverse(Acc));
+make_d3([#tscell{binary_value    = Bin,
+		 integer_value   = undefined,
+		 numeric_value   = undefined,
+		 timestamp_value = undefined,
+		 boolean_value   = undefined,
+		 set_value       = [],
+		 map_value       = undefined} | T], Cols, Acc) ->
+    make_d3(T, Cols, [Bin | Acc]);
+make_d3([#tscell{binary_value    = undefined,
+		 integer_value   = Int,
+		 numeric_value   = undefined,
+		 timestamp_value = undefined,
+		 boolean_value   = undefined,
+		 set_value       = [],
+		 map_value       = undefined} | T], Cols, Acc) ->
+    make_d3(T, Cols, [Int | Acc]);
+make_d3([#tscell{binary_value    = undefined,
+		 integer_value   = undefined,
+		 numeric_value   = Num,
+		 timestamp_value = undefined,
+		 boolean_value   = undefined,
+		 set_value       = [],
+		 map_value       = undefined} | T], Cols, Acc) ->
+    make_d3(T, Cols, [Num | Acc]);
+make_d3([#tscell{binary_value    = undefined,
+		 integer_value   = undefined,
+		 numeric_value   = undefined,
+		 timestamp_value = Timestamp,
+		 boolean_value   = undefined,
+		 set_value       = [],
+		 map_value       = undefined} | T], Cols, Acc) ->
+    make_d3(T, Cols, [Timestamp | Acc]);
+make_d3([#tscell{binary_value    = undefined,
+		 integer_value   = undefined,
+		 numeric_value   = undefined,
+		 timestamp_value = undefined,
+		 boolean_value   = Bool,
+		 set_value       = [],
+		 map_value       = undefined} | T], Cols, Acc) ->
+    make_d3(T, Cols, [Bool | Acc]);
+make_d3([#tscell{binary_value    = undefined,
+		 integer_value   = undefined,
+		 numeric_value   = undefined,
+		 timestamp_value = undefined,
+		 boolean_value   = undefined,
+		 set_value       = Set,
+		 map_value       = undefined} | T], Cols, Acc) ->
+    make_d3(T, Cols, [Set | Acc]);
+make_d3([#tscell{binary_value    = undefined,
+		 integer_value   = undefined,
+		 numeric_value   = undefined,
+		 timestamp_value = undefined,
+		 boolean_value   = undefined,
+		 set_value       = [],
+		 map_value       = Map} | T], Cols, Acc) ->
+    make_d3(T, Cols, [Map | Acc]).
