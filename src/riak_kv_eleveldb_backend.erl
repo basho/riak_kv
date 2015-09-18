77c910a9e4e1d0d1876e55f7c48b52f32cc33b08
Gordon Guthrie <gguthrie@basho.com>
2015-09-18 15:19:36 +0000

    partial fixup of calling into new eleveldb

--- src/riak_kv_eleveldb_backend.erl
+++ src/riak_kv_eleveldb_backend.erl
@@ -451,20 +451,20 @@ range_scan(FoldIndexFun, Buffer, Opts, #state{fold_opts=_FoldOpts,
     StartK2 = [{Field, Val} || {Field, _Type, Val} <- StartK],
     StartK3 = riak_ql_ddl:make_key(Mod, LK, StartK2),
     StartKey = eleveldb_ts:encode_key(StartK3),
-    StartKey2 = sext:encode({o, Bucket, StartKey}),
+    StartKey2 = to_object_key({Bucket, Bucket}, StartKey),
     EndK2 = [{Field, Val} || {Field, _Type, Val} <- EndK],
     EndK3 = riak_ql_ddl:make_key(Mod, LK, EndK2),
     EndKey = eleveldb_ts:encode_key(EndK3),
-    EndKey2 = sext:encode({o, Bucket, EndKey}),
+    EndKey2 = to_object_key({Bucket, Bucket}, EndKey),
     FoldFun = fun({K, V}, Acc) ->
 		     [{K, V} | Acc]
 	     end,
     Options = [
-	       StartKey2,
-	       EndKey2,
-	       {fold_method, streaming},
+	       {first_key,    StartKey2},
+	       {end_key,      EndKey2},
+	       {fold_method,  streaming},
 	       {range_filter, Filter},
-	       {encoding, msgpack}
+	       {encoding,     msgpack}
 	      ],
     KeyFolder = fun() ->
 			Vals = eleveldb:fold(Ref, FoldFun, [], Options),
