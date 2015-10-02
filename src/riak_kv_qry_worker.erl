999f67e5d239cc783ae271c7befd35a2a346a438
Gordon Guthrie <gguthrie@basho.com>
2015-10-02 12:33:18 +0000

    response to review comments - remove dead clause

--- src/riak_kv_qry_worker.erl
+++ src/riak_kv_qry_worker.erl
@@ -246,9 +246,6 @@ handle_side_effects([{run_sub_query, {qry, Q}, {qid, QId}} | T]) ->
     Me = self(),
     CoverageFn = {colocated, riak_kv_qry_coverage_plan},
     {ok, _PID} = riak_kv_index_fsm_sup:start_index_fsm(node(), [{raw, QId, Me}, [Bucket, none, Q, Timeout, all, undefined, CoverageFn]]),
-    handle_side_effects(T);
-handle_side_effects([H | T]) ->
-    lager:info("Not handling side effect ~p", [H]),
     handle_side_effects(T).
 
 decode_results(KVList, SelectSpec) ->
