9656039a18c524c162430cfeae06900aaa2cbb6c
Andrei Zavada <azavada@contractor.basho.com>
2016-04-11 05:26:33 +0300

    move PB error code defines to riak_kv_ts_svc (unused elsewhere)

--- src/riak_kv_ts_svc.erl
+++ src/riak_kv_ts_svc.erl
@@ -29,6 +29,32 @@
 -include("riak_kv_ts.hrl").
 -include("riak_kv_ts_svc.hrl").
 
+%% per RIAK-1437, error codes assigned to TS are in the 1000-1500 range
+-define(E_SUBMIT,            1001).
+-define(E_FETCH,             1002).
+-define(E_IRREG,             1003).
+-define(E_PUT,               1004).
+-define(E_NOCREATE,          1005).   %% unused
+-define(E_NOT_TS_TYPE,       1006).
+-define(E_MISSING_TYPE,      1007).
+-define(E_MISSING_TS_MODULE, 1008).
+-define(E_DELETE,            1009).
+-define(E_GET,               1010).
+-define(E_BAD_KEY_LENGTH,    1011).
+-define(E_LISTKEYS,          1012).
+-define(E_TIMEOUT,           1013).
+-define(E_CREATE,            1014).
+-define(E_CREATED_INACTIVE,  1015).
+-define(E_CREATED_GHOST,     1016).
+-define(E_ACTIVATE,          1017).
+-define(E_BAD_QUERY,         1018).
+-define(E_TABLE_INACTIVE,    1019).
+-define(E_PARSE_ERROR,       1020).
+-define(E_NOTFOUND,          1021).
+
+-define(FETCH_RETRIES, 10).  %% TODO make it configurable in tsqueryreq
+-define(TABLE_ACTIVATE_WAIT, 30). %% ditto
+
 -export([decode_query_common/2,
          process/2,
          process_stream/3]).
