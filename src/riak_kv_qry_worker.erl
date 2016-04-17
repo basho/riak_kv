bf62ab838b5b7eae0a746eb38e1280ac8cfd3436
Andrei Zavada <azavada@contractor.basho.com>
2016-04-17 03:26:13 +0300

    HTTP client API
    A two month worth of commits by @hmmr and @lehoff, squashed.
    
    The odd change in riak_kv_qry_worker (preserving lists for records in
    the return value of query) goes with the list_to_tuple conversion now
    happening in riak_kv_ts_api. Native lists from riak_kv_qry:submit are
    immediately consumable by mochijson2:encode, and this is the form we
    would like to have the query results in wm_timeseries_query.

--- src/riak_kv_qry_worker.erl
+++ src/riak_kv_qry_worker.erl
@@ -279,7 +279,7 @@ prepare_final_results(#state{
 prepare_final_results2(#riak_sel_clause_v1{col_return_types = ColTypes,
                                            col_names = ColNames}, Rows) ->
     %% filter out empty records
-    FinalRows = [list_to_tuple(R) || R <- Rows, R /= [[]]],
+    FinalRows = [R || R <- Rows, R /= [[]]],
     {ColNames, ColTypes, FinalRows}.
 
 %%%===================================================================
@@ -292,10 +292,8 @@ prepare_final_results2(#riak_sel_clause_v1{col_return_types = ColTypes,
 
 prepare_final_results_test() ->
     Rows = [[12, <<"windy">>], [13, <<"windy">>]],
-    RowsAsTuples = [{12, <<"windy">>}, {13, <<"windy">>}],
-    % IndexedChunks = [{1, Rows}],
     ?assertEqual(
-        {[<<"a">>, <<"b">>], [sint64, varchar], RowsAsTuples},
+        {[<<"a">>, <<"b">>], [sint64, varchar], Rows},
         prepare_final_results(
             #state{
                 qry =
