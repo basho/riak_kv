basho/riak_kv

commit e34fc1a0784319bef9b49127fb1d26a0bc62a7f4

sanitize CoverageContext parameter for query in qry_compiler:compile
Now, instead of crashing with a badarg, bad (non-binary) and doctored
values are properly diagnosed and reported.

Also, accept <<>> for Cover as substitutes for 'undefined'.

src/riak_kv_qry_compiler.erl
@@ -79,13 +79,15 @@ compile_where_clause(?DDL{} = DDL,
                                  'WHERE'       = W,
                                  cover_context = Cover} = Q,
                      MaxSubQueries) ->
-    {RealCover, WhereModifications} = unwrap_cover(Cover),
-    case compile_where(DDL, W) of
-        {error, E} -> {error, E};
-        NewW ->
+    case {compile_where(DDL, W), unwrap_cover(Cover)} of
+        {{error, E}, _} ->
+            {error, E};
+        {_, {error, E}} ->
+            {error, E};
+        {NewW, {ok, {RealCover, WhereModifications}}} ->
             expand_query(DDL, Q?SQL_SELECT{cover_context = RealCover},
-                              update_where_for_cover(NewW, WhereModifications),
-                              MaxSubQueries)
+                         update_where_for_cover(NewW, WhereModifications),
+                         MaxSubQueries)
     end.
 
 %% now break out the query on quantum boundaries
@@ -827,12 +829,19 @@ rewrite2([#param_v1{name = [FieldName]} | T], Where1, Mod, Acc) ->
 
 %% Functions to assist with coverage chunks that redefine quanta ranges
 unwrap_cover(undefined) ->
-    {undefined, undefined};
+    {ok, {undefined, undefined}};
+unwrap_cover(<<>>) ->
+    {ok, {undefined, undefined}};
 unwrap_cover(Cover) ->
-    {ok, {OpaqueContext, {FieldName, RangeTuple}}} =
-        riak_kv_pb_coverage:checksum_binary_to_term(Cover),
-    {riak_kv_pb_coverage:term_to_checksum_binary(OpaqueContext),
-     {FieldName, RangeTuple}}.
+    case catch riak_kv_pb_coverage:checksum_binary_to_term(Cover) of
+        {ok, {OpaqueContext, {FieldName, RangeTuple}}} ->
+            {riak_kv_pb_coverage:term_to_checksum_binary(OpaqueContext),
+             {FieldName, RangeTuple}};
+        {error, invalid_checksum} ->
+            {error, invalid_coverage_context_checksum};
+        {'EXIT', _} ->
+            {error, bad_coverage_context}
+    end.
 
 update_where_for_cover(Where, undefined) ->
     Where;
@@ -891,9 +900,11 @@ is_query_valid(?DDL{table = Table} = DDL, Q) ->
     riak_ql_ddl:is_query_valid(Mod, DDL, riak_kv_ts_util:sql_record_to_tuple(Q)).
 
 get_query(String) ->
+    get_query(String, undefined).
+get_query(String, Cover) ->
     Lexed = riak_ql_lexer:get_tokens(String),
     {ok, Q} = riak_ql_parser:parse(Lexed),
-    riak_kv_ts_util:build_sql_record(select, Q, undefined).
+    riak_kv_ts_util:build_sql_record(select, Q, Cover).
 
 get_long_ddl() ->
     SQL = "CREATE TABLE GeoCheckin " ++
@@ -2215,5 +2226,22 @@ negate_an_aggregation_function_test() ->
         [-3],
         finalise_aggregate(Select, [3])
       ).
+
+-define(TINY_DDL, "create table t (a timestamp not null, primary key ((quantum(a,1,d)), a))").
+-define(TINY_SQL, "select a from t where a>0 and a<2").
+
+doctored_coverage_context_test() ->
+    NotACheckSum = 34,
+    OfThisTerm = <<"f,a,f,a">>,
+    {ok, Q} = get_query(?TINY_SQL, term_to_binary({NotACheckSum, OfThisTerm})),
+    ?assertEqual(
+       {error, invalid_coverage_context_checksum},
+       compile(get_ddl(?TINY_DDL), Q, 100)).
+
+outright_bad_coverage_context_test() ->
+    {ok, Q} = get_query(?TINY_SQL, not_a_binary),
+    ?assertEqual(
+       {error, bad_coverage_context},
+       compile(get_ddl(?TINY_DDL), Q, 100)).
 
 -endif.

src/riak_kv_ts_svc.erl
@@ -440,6 +440,10 @@ sub_tsqueryreq(_Mod, DDL = ?DDL{table = Table}, SQL, State) ->
         %% these come from riak_kv_qry_compiler, even though the query is a valid SQL.
         {error, {_DDLCompilerErrType, DDLCompilerErrDesc}} when is_atom(_DDLCompilerErrType) ->
             {reply, make_rpberrresp(?E_SUBMIT, DDLCompilerErrDesc), State};
+        {error, invalid_coverage_context_checksum} ->
+            {reply, make_rpberrresp(?E_SUBMIT, "Query coverage context fails checksum"), State};
+        {error, bad_coverage_context} ->
+            {reply, make_rpberrresp(?E_SUBMIT, "Bad coverage context"), State};
 
         {error, Reason} ->
             {reply, make_rpberrresp(?E_SUBMIT, Reason), State}

src/riak_kv_wm_timeseries_query.erl
@@ -210,6 +210,10 @@ process_post(RD, #ctx{sql_type = QueryType,
             %% the eleveldb process did manage to send us a timeout
             %% response
             riak_kv_wm_ts_util:handle_error(backend_timeout, RD, Ctx);
+        {error, invalid_coverage_context_checksum} ->
+            riak_kv_wm_ts_util:handle_error({parameter_error, "Query coverage context fails checksum"}, RD, Ctx);
+        {error, bad_coverage_context} ->
+            riak_kv_wm_ts_util:handle_error({parameter_error, "Bad coverage context"}, RD, Ctx);
         {error, Reason} ->
             riak_kv_wm_ts_util:handle_error({query_exec_error, QueryType, Table, Reason}, RD, Ctx)
     end.

commit 54905b828f5dd087f70348bf3c4937ce4c055870

fix up the tuple returned from unwrap_cover in qry_compiler
fixes a regressions from e34fc1a07.

src/riak_kv_qry_compiler.erl
@@ -828,13 +828,17 @@ rewrite2([#param_v1{name = [FieldName]} | T], Where1, Mod, Acc) ->
     end.
 
 %% Functions to assist with coverage chunks that redefine quanta ranges
+-spec unwrap_cover(undefined | binary()) ->
+                          {ok, {undefined, undefined} |
+                           {OpaqueContext::binary(), {FieldName::binary(), Range::tuple()}}} |
+                          {error, invalid_coverage_context_checksum}.
 unwrap_cover(undefined) ->
     {ok, {undefined, undefined}};
 unwrap_cover(Cover) when is_binary(Cover) ->
     case catch riak_kv_pb_coverage:checksum_binary_to_term(Cover) of
         {ok, {OpaqueContext, {FieldName, RangeTuple}}} ->
-            {riak_kv_pb_coverage:term_to_checksum_binary(OpaqueContext),
-             {FieldName, RangeTuple}};
+            {ok, {riak_kv_pb_coverage:term_to_checksum_binary(OpaqueContext),
+                  {FieldName, RangeTuple}}};
         {error, invalid_checksum} ->
             {error, invalid_coverage_context_checksum}
     end.