basho/riak_kv

commit 99dff3b48c793688a0707d3832f195b0f7984e2e

don't treat Cover = <<>> as a valid substitute for undefined
with a trivial macro cleanup in doctored_coverage_context_test, and some
renaming.

src/riak_kv_qry_compiler.erl
@@ -830,8 +830,6 @@ rewrite2([#param_v1{name = [FieldName]} | T], Where1, Mod, Acc) ->
 %% Functions to assist with coverage chunks that redefine quanta ranges
 unwrap_cover(undefined) ->
     {ok, {undefined, undefined}};
-unwrap_cover(<<>>) ->
-    {ok, {undefined, undefined}};
 unwrap_cover(Cover) when is_binary(Cover) ->
     case catch riak_kv_pb_coverage:checksum_binary_to_term(Cover) of
         {ok, {OpaqueContext, {FieldName, RangeTuple}}} ->
@@ -2225,15 +2223,12 @@ negate_an_aggregation_function_test() ->
         finalise_aggregate(Select, [3])
       ).
 
--define(TINY_DDL, "create table t (a timestamp not null, primary key ((quantum(a,1,d)), a))").
--define(TINY_SQL, "select a from t where a>0 and a<2").
-
-doctored_coverage_context_test() ->
+coverage_context_not_a_tuple_or_invalid_checksum_test() ->
     NotACheckSum = 34,
     OfThisTerm = <<"f,a,f,a">>,
-    {ok, Q} = get_query(?TINY_SQL, term_to_binary({NotACheckSum, OfThisTerm})),
+    {ok, Q} = get_query("select a from t where a>0 and a<2", term_to_binary({NotACheckSum, OfThisTerm})),
     ?assertEqual(
        {error, invalid_coverage_context_checksum},
-       compile(get_ddl(?TINY_DDL), Q, 100)).
+       compile(get_ddl("create table t (a timestamp not null, primary key ((quantum(a,1,d)), a))"), Q, 100)).
 
 -endif.

commit 234670215a770fc41ed424b7ba9bbdb49028745b

Replacement coverage queries for timeseries.
* Support replacement of coverage chunks so that clients can recover
  from situations where a server is unreachable.
* New document describing the core concepts, data structures, and code
  paths.
* Support eventual cleanup of coverage chunk data once mixed clusters
  with Riak TS 1.3 are no longer supported.
    * Extensively document the changes needed at that point in time.
* Fix coverage bug introduced in #1412

src/riak_client.erl
@@ -45,7 +45,7 @@
 -export([get_client_id/1]).
 -export([for_dialyzer_only_ignore/3]).
 -export([ensemble/1]).
--export([get_cover/3, get_cover/4, get_cover/5, replace_cover/6]).
+-export([get_cover/3, get_cover/4, get_cover/5, replace_cover/6, replace_cover/7]).
 -export([vnode_target/1]).
 
 -include_lib("riak_core/include/riak_core_vnode.hrl").
@@ -534,7 +534,18 @@ get_cover_aux(Module, MinPar, ReqId, N, RingSize, Target) ->
 %% Will ignore the number of partitions, reconstruct from the coverage
 %% plan chunk we're replacing. The `Replace' argument is an ok/error
 %% tuple because it is intended to be the result of
-%% `riak_kv_pb_coverage:checksum_binary_to_term'
+%% `riak_kv_pb_coverage:checksum_binary_to_term'. For the same reason
+%% `OtherBroken' will be a list of such ok/error tuples.
+-spec replace_cover(Mod :: module(), Bucket :: binary() | {binary(), binary()},
+                    Parallelization :: undefined | non_neg_integer(),
+                    Replace :: {ok, list({atom(), term()})} |
+                               {error, Reason},
+                    OtherBroken :: list(
+                      {ok, list({atom(), term()})} |
+                      {error, Reason}),
+                    Metadata :: list(),
+                    riak_client()) ->
+                           list(list({atom(), term()})) | {error, term()}.
 -spec replace_cover(Mod :: module(), Bucket :: binary() | {binary(), binary()},
                     Parallelization :: undefined | non_neg_integer(),
                     Replace :: {ok, list({atom(), term()})} |
@@ -544,9 +555,17 @@ get_cover_aux(Module, MinPar, ReqId, N, RingSize, Target) ->
                       {error, Reason}),
                     riak_client()) ->
                            list(list({atom(), term()})) | {error, term()}.
-replace_cover(_Mod, _Bucket, _P, {error, Reason}, _OtherBroken, _Client) ->
+replace_cover(Mod, Bucket, P, Replace, OtherBroken, Client) ->
+    replace_cover(Mod, Bucket, P, Replace, OtherBroken, [], Client).
+
+replace_cover(_Mod, _Bucket, _P, {error, Reason}, _OtherBroken, _Meta, _Client) ->
     {error, Reason};
-replace_cover(Mod, Bucket, _P, {ok, Replace}, OtherBroken, _Client) ->
+replace_cover(riak_core_coverage_plan, Bucket, _P, {ok, Replace},
+              OtherBroken, _Meta, _Client) ->
+    Mod = riak_core_coverage_plan,
+    %% Core coverage plan requests can be "traditional" or
+    %% parallel. See comments in `riak_core_coverage_plan.erl' for
+    %% more details
     NVal = n_val(Bucket),
     DownNodes = extract_proplist_nodes(OtherBroken),
     case proplists:get_value(subpartition, Replace) of
@@ -556,7 +575,42 @@ replace_cover(Mod, Bucket, _P, {ok, Replace}, OtherBroken, _Client) ->
             replace_traditional_cover(Mod, NVal, Replace, DownNodes);
         _Defined ->
             replace_subpartition_cover(Mod, NVal, Replace, DownNodes)
-    end.
+    end;
+replace_cover(Mod, Bucket, _P, {ok, Replace}, OtherBroken, Meta, _Client) ->
+    %% As of 1.6 or merged KV equivalent, we should be able to
+    %% directly read these properties. The cover plan should be a
+    %% proplist natively, but John Daily messed up with 1.3.
+    %%
+    %% Other coverage module writers: make certain your coverage
+    %% chunks are simple proplists with `vnode_hash' and `node' keys,
+    %% but add `cover_to_proplist' to your coverage module as an
+    %% identity function.
+    %%
+    %% In 1.6, drop the invocation of `cover_to_proplist' because it
+    %% should simply be a proplist. See `riak_kv_ts_util' for the
+    %% place to make that change.
+    BrokenCoverProps = Mod:cover_to_proplist(Replace),
+
+    %% We maintain the `ok' tuples for `extract_proplist_nodes'.
+    DownNodeProps = lists:map(fun({ok, B}) -> {ok, Mod:cover_to_proplist(B)} end, OtherBroken),
+    DownNodes = extract_proplist_nodes(DownNodeProps),
+    NVal = n_val(Bucket),
+    replace_cover(Mod, NVal, BrokenCoverProps, DownNodes, Meta).
+
+%% For coverage plans from `riak_core', we know what kind of metadata
+%% to expect (filters for traditional plans, subpartition identifiers
+%% for parallel plans). For plans from elsewhere, we expect plans to
+%% look like traditional plans.
+replace_cover(Mod, NVal, Replace, DownNodes, Meta) ->
+    split_cover(
+      Mod:replace_chunk(
+        proplists:get_value(vnode_hash, Replace),
+        proplists:get_value(node, Replace),
+        proplists:get_value(filters, Replace, []),
+        Meta,
+        NVal,
+        mk_reqid(),
+        DownNodes)).
 
 replace_traditional_cover(Mod, NVal, Replace, DownNodes) ->
     split_cover(

src/riak_kv_qry_compiler.erl
@@ -832,9 +832,20 @@ unwrap_cover(undefined) ->
     {ok, {undefined, undefined}};
 unwrap_cover(Cover) when is_binary(Cover) ->
     case catch riak_kv_pb_coverage:checksum_binary_to_term(Cover) of
-        {ok, {OpaqueContext, {FieldName, RangeTuple}}} ->
-            {riak_kv_pb_coverage:term_to_checksum_binary(OpaqueContext),
-             {FieldName, RangeTuple}};
+        {ok, {Proplist, {FieldName, RangeTuple}}} ->
+            {ok, {riak_kv_pb_coverage:term_to_checksum_binary(Proplist),
+             {FieldName, RangeTuple}}};
+
+        %% As of 1.6 or the equivalent merged KV version, we can start
+        %% generating simpler coverage chunks for timeseries that are
+        %% pure property lists instead of a tuple with the
+        %% `vnode_hash'/`node' property list as first element and the
+        %% local where clause parameters as second.
+        {ok, Proplist} ->
+            FieldName = proplists:get_value(ts_where_field, Proplist),
+            RangeTuple = proplists:get_value(ts_where_range, Proplist),
+            {ok, {Cover, {FieldName, RangeTuple}}};
+
         {error, invalid_checksum} ->
             {error, invalid_coverage_context_checksum}
     end.

src/riak_kv_qry_coverage_plan.erl
@@ -24,10 +24,34 @@
 
 -export([
          create_plan/5,
-         create_plan/6
+         create_plan/6,
+         replace_chunk/7,
+         cover_to_proplist/1
         ]).
 
 -include("riak_kv_ts.hrl").
+
+-type index() :: chash:index_as_int().
+
+%% In `riak_kv_ts_util' we should have added the quantum boundary
+%% metadata as more keys in the native coverage proplist, but we
+%% didn't. In TS 1.6 (or merged KV equivalent) we can and should fix
+%% that and change this to an identity function (or, hopefully, remove
+%% it entirely; `riak_client' should stop calling it).
+-spec cover_to_proplist(tuple()) -> list(tuple()).
+cover_to_proplist({Proplist, _WhereBoundary}) ->
+    Proplist.
+
+-spec replace_chunk(VnodeIdx :: index(),
+                    Node :: node(),
+                    Filters :: list(index()),
+                    Metadata :: list(tuple()),
+                    NVal :: pos_integer(),
+                    ReqId :: pos_integer(),
+                    DownNodes :: list(node())) ->
+                           {error, term()} | list(list()).
+replace_chunk(_VnodeIdx, Node, _Filters, [{Query, Bucket}], NVal, ReqId, DownNodes) ->
+    do_plan({Query, Bucket}, NVal, 1, ReqId, riak_kv, undefined, [Node] ++ DownNodes).
 
 %%
 create_plan({Query, Bucket}, NVal, PVC, ReqId, NodeCheckService) ->
@@ -39,8 +63,11 @@ create_plan({Query, Bucket}, NVal, PVC, ReqId, NodeCheckService, Request) when n
     %% that's a battle for another day
     create_plan({Query, Bucket}, NVal, PVC,
                 erlang:phash2({self(), os:timestamp()}), NodeCheckService, Request);
+create_plan({Query, Bucket}, NVal, PVC, ReqId, NodeCheckService, Request) ->
+    do_plan({Query, Bucket}, NVal, PVC, ReqId, NodeCheckService, Request, []).
+
 %% PVC is generally 1 anyway, no support for it yet
-create_plan({Query, Bucket}, NVal, _PVC, ReqId, NodeCheckService, _Request) ->
+do_plan({Query, Bucket}, NVal, _PVC, ReqId, NodeCheckService, _Request, DownNodes) ->
     {ok, CHBin} = riak_core_ring_manager:get_chash_bin(),
     RingSize = chashbin:num_partitions(CHBin),
     UnavailableVNodes =
@@ -52,7 +79,10 @@ create_plan({Query, Bucket}, NVal, _PVC, ReqId, NodeCheckService, _Request) ->
         [] ->
             {error, no_primaries_available};
         VNodes when is_list(VNodes) ->
-            AvailVNodes = filter_down_vnodes(VNodes, UnavailableVNodes, RingSize),
+            AvailVNodes = drop_nodes(
+                            filter_down_vnodes(VNodes, UnavailableVNodes, RingSize),
+                            DownNodes),
+
             case AvailVNodes of
                 [] ->
                     {error, no_primaries_available};
@@ -77,6 +107,12 @@ filter_down_vnodes(VNodes, Unavail, RingSize) ->
     RingIndexInc = chash:ring_increment(RingSize),
     VNodes -- lists:map(fun({vnode_id, Id, Node}) -> {Id * RingIndexInc, Node} end,
                         Unavail).
+
+%% Take the results from filter_down_vnodes/3 and drop any vnodes on
+%% nodes that the client believes to be unavailable
+drop_nodes(Vnodes, DownNodes) ->
+    lists:filter(fun({_Index, Node}) -> not lists:member(Node, DownNodes) end,
+                 Vnodes).
 
 %% This is fugly because the physical format of the startkey
 %% which is neede by eleveldb is being used by the query

src/riak_kv_ts_svc.erl
@@ -131,8 +131,6 @@ process(M = #tsdelreq{table = Table}, State) ->
 process(M = #tslistkeysreq{table = Table}, State) ->
     check_table_and_call(Table, fun sub_tslistkeysreq/4, M, State);
 
-%% No support yet for replacing coverage components; we'll ignore any
-%% value provided for replace_cover
 process(M = #tscoveragereq{table = Table}, State) ->
     check_table_and_call(Table, fun sub_tscoveragereq/4, M, State);
 
@@ -380,18 +378,24 @@ sub_tslistkeysreq(Mod, DDL, #tslistkeysreq{table = Table,
 %% -----------
 
 sub_tscoveragereq(Mod, _DDL, #tscoveragereq{table = Table,
-                                            query = Q},
+                                            query = Q,
+                                            replace_cover=R,
+                                            unavailable_cover=U},
                   State) ->
     Client = {riak_client, [node(), undefined]},
     %% all we need from decode_query is to compile the query,
     %% but also to check permissions
     case decode_query(Q, undefined) of
         {_QryType, {ok, SQL}} ->
-            case riak_kv_ts_api:compile_to_per_quantum_queries(Mod, SQL) of
+            %% Make sure, if we pass a replacement cover, we use it to
+            %% determine the proper where range
+            case riak_kv_ts_api:compile_to_per_quantum_queries(Mod,
+                                                               SQL?SQL_SELECT{cover_context=R}) of
                 {ok, Compiled} ->
                     Bucket = riak_kv_ts_util:table_to_bucket(Table),
                     {reply,
-                     {tscoverageresp, riak_kv_ts_util:sql_to_cover(Client, Compiled, Bucket, [])},
+                     {tscoverageresp,
+                      riak_kv_ts_util:sql_to_cover(Client, Compiled, Bucket, R, U, [])},
                      State};
                 {error, Reason} ->
                     {reply, make_rpberrresp(

src/riak_kv_ts_util.erl
@@ -36,7 +36,7 @@
          pk/1,
          queried_table/1,
          sql_record_to_tuple/1,
-         sql_to_cover/4,
+         sql_to_cover/6,
          table_to_bucket/1,
          validate_rows/2,
          row_to_key/3
@@ -398,9 +398,9 @@ row_to_key(Row, DDL, Mod) ->
 
 %% If any of the results from get_cover are errors, we want that tuple
 %% to be the sole return value
-sql_to_cover(_Client, [], _Bucket, Accum) ->
+sql_to_cover(_Client, [], _Bucket, _Replace, _Unavail, Accum) ->
     lists:reverse(Accum);
-sql_to_cover(Client, [SQL|Tail], Bucket, Accum) ->
+sql_to_cover(Client, [SQL|Tail], Bucket, undefined, _Unavail, Accum) ->
     case Client:get_cover(riak_kv_qry_coverage_plan, Bucket, undefined,
                           {SQL, Bucket}) of
         {error, Error} ->
@@ -409,10 +409,60 @@ sql_to_cover(Client, [SQL|Tail], Bucket, Accum) ->
             {Description, SubRange} = reverse_sql(SQL),
             Node = proplists:get_value(node, Cover),
             {IP, Port} = riak_kv_pb_coverage:node_to_pb_details(Node),
+
+            %% As of 1.3, coverage chunks returned to the client for
+            %% KV are a simple proplist, but for TS are a tuple with
+            %% the first element the proplist, and the 2nd element a
+            %% representation of the quantum to which this chunk
+            %% applies.
+            %%
+            %% The fact that the structures are unnecessarily
+            %% different results in some awkwardness when interpreting
+            %% them. See `riak_client:replace_cover' for an example.
+            %%
+            %% As of 1.6 or post-merge equivalent (so that mixed
+            %% clusters with 1.3 are not supported), the SubRange
+            %% value should be added to the cover proplist instead of
+            %% creating this clumsy tuple so that cover chunks are
+            %% simple proplists for all types of coverage queries.
+            %%
+            %% See also `riak_kv_qry_compiler:unwrap_cover/1' for the
+            %% code that will interpret the proplists properly as of
+            %% 1.4 and later.
+            %%
+            %% The 2nd field in the tuple that is returned by
+            %% `reverse_sql/1' is itself a tuple with two elements:
+            %% the name of the quantum field and the relevant range
+            %% for this chunk.
+            %%
+            %% So the code to implement with 1.6 would be roughly:
+            %% Context = term_to_checksum_binary(Cover ++
+            %%     [{ts_where_field, element(1, SubRange)},
+            %%      {ts_where_range, element(2, SubRange)}]).
+            %%
+            %% (Real code would be pattern-matchy and generally cleaner.)
+
             Context = riak_kv_pb_coverage:term_to_checksum_binary({Cover, SubRange}),
             Entry = {{IP, Port}, Context, SubRange, Description},
-            sql_to_cover(Client, Tail, Bucket, [Entry|Accum])
+            sql_to_cover(Client, Tail, Bucket, undefined, [], [Entry|Accum])
+    end;
+sql_to_cover(Client, [SQL|Tail], Bucket, Replace, Unavail, Accum) ->
+    case Client:replace_cover(riak_kv_qry_coverage_plan, Bucket, undefined,
+                              riak_kv_pb_coverage:checksum_binary_to_term(Replace),
+                              lists:map(fun riak_kv_pb_coverage:checksum_binary_to_term/1,
+                                        Unavail),
+                              [{SQL, Bucket}]) of
+        {error, Error} ->
+            {error, Error};
+        [Cover] ->
+            {Description, SubRange} = reverse_sql(SQL),
+            Node = proplists:get_value(node, Cover),
+            {IP, Port} = riak_kv_pb_coverage:node_to_pb_details(Node),
+            Context = riak_kv_pb_coverage:term_to_checksum_binary({Cover, SubRange}),
+            Entry = {{IP, Port}, Context, SubRange, Description},
+            sql_to_cover(Client, Tail, Bucket, Replace, Unavail, [Entry|Accum])
     end.
+
 
 
 %% Generate a human-readable description of the target