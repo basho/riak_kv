4a7491d61b211181f29fdac6d2b1d294317f3ea7
Brian Sparrow <bsparrow@basho.com>
2016-09-29 12:02:27 -0400

    Minor fix

--- src/riak_kv_entropy_manager.erl
+++ src/riak_kv_entropy_manager.erl
@@ -568,8 +568,8 @@ do_get_build_token(State=#state{build_tokens=Tokens}) ->
             build_limit_reached
     end.
 
--spec maybe_get_build_token(state()) -> build_limit_reached | {ok, state()}.
-maybe_get_build_token(State=#state{trees_version=VTrees}) ->
+-spec maybe_get_upgrade_token(state()) -> build_limit_reached | {ok, state()}.
+maybe_get_upgrade_token(State=#state{trees_version=VTrees}) ->
     case [Idx || {Idx, Version} <- VTrees, Version /= legacy] of
         %% All legacy trees, allow build token
         [] ->
