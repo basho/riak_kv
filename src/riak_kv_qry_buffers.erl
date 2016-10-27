2ea3998818bb801a21143193a20f08daed5ae00c
Andrei Zavada <azavada@contractor.basho.com>
2016-10-27 17:08:09 +0300

    qry_buffers: double-ensure make_qbuf_id includes unique timestamps

--- src/riak_kv_qry_buffers.erl
+++ src/riak_kv_qry_buffers.erl
@@ -633,6 +633,10 @@ sql_to_ddl(Table, CompiledSelect, CompiledOrderBy) ->
          local_key     = make_lk_from_orderby(CompiledOrderBy)}.
 
 make_qbuf_id(From, Select, OrderBy) ->
+    %% In order to emulate erlang:now/0 behaviour for generating
+    %% unique timestamps, we can use the fact that the call chains
+    %% eventually involving this function are serialized:
+    timer:sleep(1),
     list_to_binary(
       fmt("~s_~s_~s__~s", [From, join_fields(Select), join_fields(OrderBy), tstamp()])).
 
