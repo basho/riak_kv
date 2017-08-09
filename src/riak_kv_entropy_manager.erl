51729b3c89081b561fcb826c24ee7bb3b17aae38
Martin Sumner <martin.sumner@adaptip.co.uk>
2017-08-09 14:24:26 +0100

    Pokes per tick
    It seemed aggressive to do ten pokes for every pick.  Also want to balance a little the fact that rebuilds don't now require a token.
    
    So, finger in air, opted for two pokes per tick.

--- src/riak_kv_entropy_manager.erl
+++ src/riak_kv_entropy_manager.erl
@@ -98,6 +98,7 @@
 -define(DEFAULT_AAE_THROTTLE_LIMITS,
         [{-1,0}, {200,10}, {500,50}, {750,250}, {900,1000}, {1100,5000}]).
 -define(ETS, entropy_manager_ets).
+-define(POKES_PER_TICK, 2).
 
 %%%===================================================================
 %%% API
@@ -667,8 +668,10 @@ tick(State) ->
     State2 = maybe_reload_hashtrees(Ring, State1),
     State3 = maybe_start_upgrade(Ring, State2),
     State4 = lists:foldl(fun(_,S) ->
-                                 maybe_poke_tree(S)
-                         end, State3, lists:seq(1,10)),
+                                maybe_poke_tree(S)
+                            end, 
+                            State3, 
+                            lists:seq(1, ?POKES_PER_TICK)),
     State5 = maybe_exchange(Ring, State4),
     State5.
 
