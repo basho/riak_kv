7cce05ade60593374e1587a14b879fe4e3108c09
Russell Brown <russell.brown@mac.com>
2018-10-09 09:45:56 +0100

    Add capability for leveled/vnode head command
    For mixed cluster/upgrade usage

--- src/riak_kv_app.erl
+++ src/riak_kv_app.erl
@@ -202,6 +202,11 @@ start(_Type, _StartArgs) ->
                                           [enabled, disabled],
                                           disabled),
 
+            riak_core_capability:register({riak_kv, get_request_type},
+                                          [head, get],
+                                          get),
+
+
             HealthCheckOn = app_helper:get_env(riak_kv, enable_health_checks, false),
             %% Go ahead and mark the riak_kv service as up in the node watcher.
             %% The riak_core_ring_handler blocks until all vnodes have been started
