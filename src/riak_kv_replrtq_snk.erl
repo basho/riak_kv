cddf3e7aaf038b5adf98b4467ff9b9860f30f68b
Martin Sumner <martin.sumner@adaptip.co.uk>
2019-09-05 22:54:05 +0100

    Attempt to reuse PB socket

--- src/riak_kv_replrtq_snk.erl
+++ src/riak_kv_replrtq_snk.erl
@@ -373,14 +373,13 @@ determine_workitems(QueueName, PeerInfo, WorkerCount) ->
                 case Protocol of
                     http ->
                         HTC = rhc:create(Host, Port, "riak", []),
-                        fun(QN) -> rhc:fetch(HTC, QN) end;
+                        {HTC, fun rhc:fetch/2, Host, Port};
                     pb ->
-                        fun(QN) ->
-                            {ok, Pid} = riakc_pb_socket:start(Host, Port),
+                        fun(QN, CurrentPid) ->
+                            PBC =
+                                check_current_pbclient(CurrentPid, Host, Port),
                             QNB = atom_to_binary(QN, utf8),
-                            R = riakc_pb_socket:fetch(Pid, QNB),
-                            riakc_pb_socket:stop(Pid),
-                            R
+                            riakc_pb_socket:fetch(PBC, QNB)
                         end
                 end,
             {{QueueName, PeerID}, LocalClient, RemoteClientFun}
@@ -392,6 +391,20 @@ determine_workitems(QueueName, PeerInfo, WorkerCount) ->
                     lists:seq(1, WorkerCount)),
     {length(WorkItems) - WorkerCount, WorkItems}.
 
+
+check_current_pbclient(Pid, Host, Port) ->
+    case is_pid(Pid) of
+        true ->
+            case is_process_alive(Pid) of
+                true ->
+                    Pid;
+                _ ->
+                    check_current_pbclient(dummy, Host, Port)
+            end;
+        _ ->
+            {ok, NewPid} = riakc_pb_socket:start(Host, Port),
+            NewPid
+    end.
 
 %% @doc
 %% For an item of work which has been removed from the work queue, spawn a
@@ -450,8 +463,8 @@ repl_fetcher(WorkItem) ->
         end
     catch
         Type:Exception ->
-            lager:warning("Snk worker failed due to ~w:~w with trace ~w", 
-                            [Type, Exception, erlang:get_stacktrace()]),
+            lager:warning("Snk worker failed due to ~w exception ~w", 
+                            [Type, Exception]),
             done_work(WorkItem, false, {error, Type, Exception})
     end.
 
