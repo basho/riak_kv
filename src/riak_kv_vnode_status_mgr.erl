058fa67a0535830e11bf7cd2881bbfd8e37984a7
Martin Sumner <martin.sumner@adaptip.co.uk>
2021-03-19 15:45:19 +0000

    Test chmod existence (man)

--- src/riak_kv_vnode_status_mgr.erl
+++ src/riak_kv_vnode_status_mgr.erl
@@ -529,6 +529,8 @@ vnode_status_test_() ->
       ?_test(begin % update failure
                 TestPath = riak_kv_test_util:get_test_dir("kv_vnode_status_test"),
                 ?cmd("chmod a-r " ++ TestPath ++ "/0"),
+                io:format(user, "TestPath: ~s", [TestPath]),
+                ?assertEqual(ok, ?cmd("man chmod")),
                 Index = 0,
                 File = vnode_status_filename(Index, TestPath),
                 ?assertEqual({ok, []},  read_vnode_status(File))
